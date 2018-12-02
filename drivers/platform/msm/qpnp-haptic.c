c1c40ea2911db0e267f08c7e8268afd5497c7446
tanish2k09 <tanish2k09.dev@gmail.com>
2018-12-02 21:52:47 +0000

    Haptic : Add haptic feedback function
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -404,6 +404,10 @@ struct qpnp_hap {
 
 static struct qpnp_hap *ghap;
 
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+static struct qpnp_hap *vib_dev;
+#endif
+
 /* helper to read a pmic register */
 static int qpnp_hap_read_reg(struct qpnp_hap *hap, u8 *data, u16 addr)
 {
@@ -1836,6 +1840,15 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 	schedule_work(&hap->td_work);
 }
 
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+void set_vibrate(int value)
+{
+	if (value > 0)
+		qpnp_hap_td_enable(&vib_dev->timed_dev, value);
+	return;
+}
+#endif
+
 /* play pwm bytes */
 int qpnp_hap_play_byte(u8 data, bool on)
 {
@@ -2772,6 +2785,10 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 	}
 
 	ghap = hap;
+	
+	#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+	vib_dev = hap;
+	#endif
 
 	return 0;
 
