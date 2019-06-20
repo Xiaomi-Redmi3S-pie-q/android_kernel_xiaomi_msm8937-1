8c79a0ac550198ff3291d0b9e9661e0dd616af41
JacobDeepu <d2jdeepu@gmail.com>
2019-06-20 12:05:08 +0000

    input:dt2w: Fix double tap to wake Switch in settings
    Signed-off-by: JacobDeepu <d2jdeepu@gmail.com>

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -62,10 +62,15 @@ MODULE_LICENSE("GPLv2");
 #define DT2W_TIME		400
 #define VIB_STRENGTH	20
 
+#define DT2W_OFF 0
+#define DT2W_ON 1
+#define DT2W_FS 2
+
 /* Resources */
 extern void set_vibrate(int value);
 static int dt2w_vib_strength = VIB_STRENGTH;
 int dt2w_switch = DT2W_DEFAULT;
+bool dt2w_scr_suspended = false;
 static cputime64_t tap_time_pre = 0;
 static int touch_x = 0, touch_y = 0, touch_nr = 0, x_pre = 0, y_pre = 0;
 static bool touch_x_called = false, touch_y_called = false, touch_cnt = true;
@@ -289,11 +294,30 @@ static ssize_t dt2w_doubletap2wake_show(struct device *dev,
 static ssize_t dt2w_doubletap2wake_dump(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	if (buf[0] >= '0' && buf[0] <= '2' && buf[1] == '\n')
-                if (dt2w_switch != buf[0] - '0')
-		        dt2w_switch = buf[0] - '0';
+	unsigned int new_dt2w_switch;
+       if (!sscanf(buf, "%du", &new_dt2w_switch))
+		return -EINVAL;
 
-	return count;
+	if (new_dt2w_switch == dt2w_switch)
+		return count;
+
+	switch (new_dt2w_switch) {
+		case DT2W_OFF :
+		case DT2W_ON :
+		case DT2W_FS :
+			if (dt2w_scr_suspended) {
+				pr_info("[cmdline_dt2w]: DoubleTap2Wake isn't changed because the screen is off.'\n");
+				return count;
+			}
+			dt2w_switch = new_dt2w_switch;
+			pr_info("[dump_dt2w]: DoubleTap2Wake toggled. | "
+					"dt2w='%d' \n", dt2w_switch);
+			return count;
+		default:
+			return -EINVAL;
+	}
+
+	return -EINVAL;
 }
 
 static DEVICE_ATTR(doubletap2wake, (S_IWUSR|S_IRUGO),
